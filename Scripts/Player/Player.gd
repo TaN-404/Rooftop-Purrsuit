1220c4478c81dfdf2b9e8ed13af42c0eaa04fb71
TaN-404 <103211138+TaN-404@users.noreply.github.com>
2023-12-12 00:16:45 +0530

    added cat animation

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 500
-const JUMP_VELOCITY = -400.0
+const SPEED = 700
+const JUMP_VELOCITY = -600.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
